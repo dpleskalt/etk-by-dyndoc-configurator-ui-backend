cada3734ec5a65f1e8406c23873fcc5b0e0a607e
Dzmitry Harlach <dzmitry.harlach@ericsson.com>
2022-04-01 17:31:56 +0300

    JIRA: EEB-10107 Deploy dyndoc-configurator to the lab environment.
    - added configuration to the lab environment.
    - removed unnecessary code from initial java-template

--- src/main/java/hr/ericsson/ehealth/belarus/dyndoc/configurator/repository/DyndocRepository.java
+++
@@ -1,7 +0,0 @@
-package hr.ericsson.ehealth.belarus.dyndoc.configurator.repository;
-
-import hr.ericsson.ehealth.belarus.dyndoc.configurator.dto.DyndocDto;
-
-public interface DyndocRepository {
-  public DyndocDto fetchData();
-}
